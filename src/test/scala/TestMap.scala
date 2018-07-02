00a4e3f13fbbc58effbb55bc7c6cd144b36abc79
Julián Carvajal Montoya <julian@MacBook-Air-de-Julian.local>
2018-07-01 21:46:15 -0500

    Tests Map

--- src/test/scala/TestMap.scala
+++ src/test/scala/TestMap.scala
@@ -1,3 +1,5 @@
+import java.util.NoSuchElementException
+
 import org.scalatest._
 import org.scalatest.Matchers._
 
@@ -29,9 +31,45 @@ class TestMap extends FunSuite {
   {
     val mapa = Map(1->"Enero",2->"Febrero",3->"Marzo")
     val mapa2 = mapa+(1->"Diciembre")
+    val listaValues = mapa2.values
+
 
     mapa2(1) shouldBe "Diciembre"
+    listaValues.filter(x=>x=="Enero").size should be (0)
 
   }
+
+  test("Buscar elemento que no existe")
+  {
+    val mapa = Map(1->"Enero",2->"Febrero",3->"Marzo")
+    assertThrows[NoSuchElementException]{mapa(5)}
+    assertResult("Abril"){mapa.getOrElse(5,"Abril")}
+
+  }
+
+  test("Buscar elemento que no existe 2")
+  {
+    val mapa = Map(1->"Enero",2->"Febrero",3->"Marzo") withDefault("holas")
+    val mapa2 = Map(1->"Enero",2->"Febrero",3->"Marzo") withDefaultValue ("Falta un dato2")
+
+    mapa(4) shouldBe 's'
+    mapa2(4) shouldBe "Falta un dato2"
+
+  }
+
+  test("eliminar elementos de un mapa"){
+    val mapa = Map(1->"Enero",2->"Febrero",3->"Marzo")
+    val mapa2 = mapa - 1
+    mapa.contains(1) shouldBe true
+    mapa2.contains(1) shouldBe false
+  }
+
+
+
+
+
+
+
+
 
 }
